aa3231c0892d1a5fb6cdfc5334c44a4eb8bb23c0
Niraj Sapkota <n.sapkota@outlook.com.au>
2026-04-30 11:47:47 +1000

    Add: longest-substring-without-duplicates - submission-10

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-10.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        auto seen = std::unordered_map<char, int>();
+        auto longest = 0;
+        auto l = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            auto ch= s[r];
+            if (seen.contains(ch) && seen.at(ch) >= l) {
+                l = seen.at(ch) + 1;
+                seen.erase(ch);
+                seen.insert({ ch, r });
+            }
+            seen[ch] = r;
+            longest = std::max(longest, r - l + 1);
+        }
+        return longest;
+    }
+};
