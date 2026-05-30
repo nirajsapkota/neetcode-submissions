66b7e9717085ea606f1d6e4af90c499cbd20386c
Niraj Sapkota <n.sapkota@outlook.com.au>
2026-05-30 11:01:35 +1000

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        auto graph = std::unordered_map<int, std::vector<int>>();
+        for (auto const& edge : edges) {
+            auto a = edge[0];
+            auto b = edge[1];
+            graph[a].push_back(b);
+            graph[b].push_back(a);
+        }
+        
+        auto components = std::vector<std::unordered_set<int>>();
+        for (int i = 0; i < n; i++) {
+            auto explored = dfs(graph, i);
+
+            auto exists = false;
+            for (auto const& component : components) {
+                if (explored == component) {
+                    exists = true;
+                    break;
+                }
+            }
+
+            if (!exists) {
+                components.push_back(explored);
+            }
+        }
+
+        return components.size();
+    }
+
+    auto dfs(std::unordered_map<int, std::vector<int>>& graph, int start) -> std::unordered_set<int> {
+        auto s = std::stack<std::pair<int, int>>();
+        auto v = std::unordered_set<int>();
+        s.push({ start, -1 });
+        while (!s.empty()) {
+            auto [current, parent] = s.top();
+            s.pop();
+            if (v.contains(current)) continue;
+            v.insert(current);
+            for (auto const& neighbor : graph[current]) {
+                if (neighbor == parent) continue;
+                s.push({ neighbor, current });
+            }
+        }
+        return v;
+    }
+};
